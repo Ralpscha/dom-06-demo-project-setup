5c8e48ac69d1ce6a8c7a89b2b15e22e95adc9395
Ralph van Schaik <ralph.van.schaik@me.com>
2020-11-21 12:57:06 +0100

    tm 173 controlling the backdrop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,11 +3,16 @@ const addMovieModal = document.getElementById('add-modal');
 // const addMovieModal = document.body.children[1];
 const startAddMovieButton = document.querySelector("header button");
 // const startAddMovieButton = document.querySelector('header').lastElementChild;
+const backdrop = document.getElementById('backdrop');
+// const backdrop = document.body.firstElementChild;
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible');
+}
 
 const toggleMovieModal = () => {
   addMovieModal.classList.toggle('visible');
+  toggleBackdrop();
 };
-
-
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
