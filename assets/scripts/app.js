3324df6342a775585364984c19ef7fc9bc070d83
Ralph van Schaik <ralph.van.schaik@me.com>
2020-11-22 12:28:58 +0100

    tm 175 clearing usrInput

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,6 +10,7 @@ const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
 // const userInputs = addMovieModal.getElementsByTagName('input');
+const movies = [];
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle('visible');
@@ -20,26 +21,45 @@ const toggleMovieModal = () => {
   toggleBackdrop();
 };
 
-const backdropClickHandler = () => {
-  toggleMovieModal();
-};
+const clearMovieInput = () => {
+  for(const usrInput of userInputs) {
+    usrInput.value = '';
+  }
+}
 
 const cancelAddMovieHandler = () => {
   toggleMovieModal();
+  clearMovieInput();
 };
 
 const addMovieHandler = () => {
   const titleValue = userInputs[0].value;
-  const imageValue = userInputs[1].value;
+  const imageUrlValue = userInputs[1].value;
   const ratingValue = userInputs[2].value;
 
-  if(titleValue.trim() === '' || imageValue.trim() === '' || ratingValue.trim() === '' ||
+  if(titleValue.trim() === '' || imageUrlValue.trim() === '' || ratingValue.trim() === '' ||
     +ratingValue < 1 ||
       +ratingValue >5
   ) {
       alert('Please enter valid values (ratings between 1 and 5).');
       return;
   }
+
+  const newMovie = {
+     title: titleValue,
+     image: imageUrlValue,
+      rating: ratingValue
+    };
+
+  movies.push(newMovie);
+  console.log(movies);
+  clearMovieInput();
+  toggleMovieModal();
+
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
 };
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
